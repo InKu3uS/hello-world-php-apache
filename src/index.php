2684f613e48b34bc0ccaba24c2d89548aaef06c9
Nefta <Neftarguez@gmail.com>
2022-01-24 18:03:37 +0000

    cambios

--- src/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo "<h1>Proyecto de prueba</h1>";
-    echo "<p>Neftalí</p>";
-    echo "<img src='https://img.freepik.com/vector-gratis/extranjero-lindo-ejemplo-icono-vector-historieta-traje-astronauta-concepto-icono-tecnologia-ciencia-aislado-vector-premium-estilo-dibujos-animados-plana_138676-3537.jpg?size=338&ext=jpg'>";
-?>
